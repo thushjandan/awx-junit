9d2c578d3daa126b1f210f9ee2902864007f9319
Thushjandan <thushjandan@ponnudurai.ch>
2021-02-20 21:30:04 +0000

    add publish workflow.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="awx-junit",
-    version="1.0",
+    version="1.1",
     entry_points={"console_scripts": ["awx-junit=awx_junit.awx_junit:main"]},
     author="Thushjandan Ponnudurai",
     author_email="thushjandan@gmail.com",
