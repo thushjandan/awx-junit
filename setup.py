21d29c0d841017ae60a1a423b6fdccdbcaac4373
Thushjandan <thushjandan@ponnudurai.ch>
2021-02-20 19:50:04 +0000

    fixing linting issues.

--- setup.py
+++ setup.py
@@ -4,28 +4,28 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='awx-junit',  
-    version='1.0',
-    entry_points = {
-        'console_scripts': ['awx-junit=awx_junit.awx_junit:main']
-    },
+    name="awx-junit",
+    version="1.0",
+    entry_points={"console_scripts": ["awx-junit=awx_junit.awx_junit:main"]},
     author="Thushjandan Ponnudurai",
     author_email="thushjandan@gmail.com",
-    description="A Ansible Tower or AWX utility to generate a JUnit XML report from a job.",
+    description=(
+        "A Ansible Tower or AWX utility " "to generate a JUnit XML report from a job."
+    ),
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/thushjandan/awx-junit",
     packages=setuptools.find_packages(),
     install_requires=[
-       'junit-xml',
+        "junit-xml",
     ],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development :: Build Tools",
         "Topic :: Software Development :: Testing",
     ],
- )
+)
